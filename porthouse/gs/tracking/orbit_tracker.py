f4491579b42dbffe4d92623f47463cafae45a710
emeraldtip <emeraldtipyt@gmail.com>
2026-06-30 14:48:35 +0300

    Fix loss?

--- porthouse/gs/tracking/orbit_tracker.py
+++ porthouse/gs/tracking/orbit_tracker.py
@@ -108,7 +108,7 @@ class OrbitTracker(SkyfieldModuleMixin, BaseModule):
             await asyncio.sleep(0)
             return self._get_status_message()
         elif request_name == "rpc.loss":
-            await self.send_event("los", task_name=self.task_name, target=self.target, rotators=())
+            await self.publish(exchange="event", routing_key="los")
 
         elif request_name == "rpc.get_target_position":
             target_name = request_data.get("target", "")
